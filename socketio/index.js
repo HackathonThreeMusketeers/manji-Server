92165e85a617cc31bd78dca16368c311457aef3e
Ubuntu <ubuntu@ip-172-31-17-222.us-east-2.compute.internal>
2018-05-12 16:03:03 +0000

    socketio

--- socketio/index.js
+++ socketio/index.js
@@ -2,6 +2,7 @@ module.exports = function(server) {
   var io = require('socket.io')(server);
 
   io.on('connection', function(socket) {
+    console.log('connected');
     socket.on('message', function(msg) {
     console.log('message: ' + msg);
       io.emit("chat", stdout);
